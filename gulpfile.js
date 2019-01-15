5f526500339cf5baca0a6038c731609fa528a920
avarghese <arun.varghese@masergy.com>
2019-01-15 16:26:00 -0600

    updated gulp deps

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,6 @@ let beautify = require('gulp-jsbeautifier');
 let cleanCSS = require('gulp-clean-css');
 let tsProject = ts.createProject('tsconfig.json');
 let sourcemaps = require('gulp-sourcemaps');
-let runSequence = require('run-sequence');
 let browserSync = require('browser-sync').create();
 let autoprefixer = require('gulp-autoprefixer');
 let imagemin = require('gulp-imagemin');
@@ -21,219 +20,246 @@ let jade = require('gulp-jade');
 let htmlmin = require('gulp-htmlmin');
 
 let paths = {
-    scss: 'assets/css/avarghese.scss',
-    css: 'assets/css/avarghese.css',
-    ts: 'assets/js/avarghese.ts',
-    js: 'assets/js/avarghese.js',
-    jade: 'assets/html/index.jade',
-    html: 'assets/html/index.html'
+  scss: 'assets/css/avarghese.scss',
+  css: 'assets/css/avarghese.css',  
+  ts:'assets/js/avarghese.ts',
+  js:'assets/js/avarghese.js',
+  jade: 'assets/html/index.jade',
+  html: 'assets/html/index.html'
 }
 
-let banner = ['/*\n',
-    ' * <%= pkg.title %> (<%= pkg.homepage %>)\n',
-    ' * Copyright 2016-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-    ' */\n',
-    ''
+const banner = ['/*\n',
+  ' * <%= pkg.title %> (<%= pkg.homepage %>)\n',
+  ' * Copyright 2016-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+  ' */\n',
+  ''
 ].join('');
 
 /** Miscellaneous Tasks **/
 
-gulp.task('jshint', () => {
-    return gulp.src([
-            paths.ts,
-            '*.js'
-        ])
-        .pipe(plumber())
-        .pipe(jshint())
-        .pipe(jshint.reporter(stylish))
-        .pipe(jshint.reporter('fail'));
-});
+function jshintReport(done) {
+  gulp.src([
+      paths.ts,
+      '*.js'
+    ])
+    .pipe(plumber())
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish))
+    .pipe(jshint.reporter('fail'));
 
-gulp.task('format', () => {
-    return gulp.src([
-            paths.scss,
-            paths.ts,
-            'index.html',
-            '*.{js,json}'
-        ], {
-            base: './'
-        })
-        .pipe(plumber())
-        .pipe(beautify())
-        .pipe(gulp.dest('./'));
-});
+    done();
+};
+
+function format(done) {
+  gulp.src([
+       paths.scss,
+       paths.ts,
+      'index.html',
+      '*.{js,json}'
+    ], {
+      base: './'
+    })
+    .pipe(plumber())
+    .pipe(beautify())
+    .pipe(gulp.dest('./'));
+
+    done();
+};
 
 /** Image Tasks **/
 
-gulp.task('minify-images', () => {
-    gulp.src('assets/img/**/*')
-        .pipe(plumber())
-        .pipe(imagemin())
-        .pipe(gulp.dest('assets/img'))
-});
+function minifyImages(done) {
+  gulp.src('assets/img/**/*')
+    .pipe(plumber())
+    .pipe(imagemin())
+    .pipe(gulp.dest('assets/img'));
+
+    done();
+};
 
 /** JavaScript Tasks **/
 
-gulp.task('clean-js', () => {
-    return del.sync([
-        'assets/js/*.min.js',
-        'assets/js/*.map'
-    ], {
-        force: true
-    });
-});
+function cleanJs(done) {
+  del.sync([
+    'assets/js/*.min.js',
+    'assets/js/*.map'
+  ], {
+    force: true
+  });
 
-gulp.task('typescript', () => {
-    tsProject.src()
-        .pipe(plumber())
-        .pipe(tsProject())
-        .js.pipe(gulp.dest('assets/js'));
-});
+  done();
+};
 
-gulp.task('minify-js', () => {
-    return gulp.src([
-            paths.js
-        ])
-        .pipe(plumber())
-        .pipe(uglify())
-        .pipe(header(banner, {
-            pkg: pkg
-        }))
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest('assets/js'));
-});
+function typescript(done) {
+  tsProject.src()
+    .pipe(plumber())
+    .pipe(tsProject())
+    .js.pipe(gulp.dest('assets/js'));
 
-gulp.task('sourcemap-js', () => {
-    return gulp.src([
-            'assets/js/*.min.js',
-            '!assets/js/*spec.js'
-        ])
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('assets/js'));
-});
+    done();
+};
+
+function minifyJs(done) {
+  gulp.src([
+       paths.js
+    ])
+    .pipe(plumber())
+    .pipe(uglify())
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('assets/js'));
+
+    done();
+};
+
+function sourcemapJs(done) {
+  gulp.src([
+      'assets/js/*.min.js',
+      '!assets/js/*spec.js'
+    ])
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('assets/js'));
+
+    done();
+};
 
 /** CSS Tasks **/
 
-gulp.task('clean-css', () => {
-    return del.sync([
-        'assets/css/*.min.css',
-        'assets/css/*.map'
-    ], {
-        force: true
-    });
-});
+function cleanCss(done) {
+  del.sync([
+    'assets/css/*.min.css',
+    'assets/css/*.map'
+  ], {
+    force: true
+  });
 
-gulp.task('scss', () => {
-    return gulp.src('assets/css/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
-        }))
-        .pipe(gulp.dest('assets/css'));
-});
+  done();
+};
 
-gulp.task('minify-css', ['scss'], () => {
-    return gulp.src([
-            paths.css
-        ])
-        .pipe(plumber())
-        .pipe(cleanCSS({
-            compatibility: 'ie8'
-        }))
-        .pipe(header(banner, {
-            pkg: pkg
-        }))
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest('assets/css'));
-});
+function scss(done) {
+  gulp.src('assets/css/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+    }))
+    .pipe(gulp.dest('assets/css'));
 
-gulp.task('sourcemap-css', () => {
-    return gulp.src([
-            'assets/css/*.min.css'
-        ])
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('assets/css'));
-});
+    done();
+};
+
+function minifyCss(done) {
+  gulp.src([
+       paths.css
+    ])
+    .pipe(plumber())
+    .pipe(cleanCSS({
+      compatibility: 'ie8'
+    }))
+    .pipe(header(banner, {
+      pkg: pkg
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('assets/css'));
+
+    done();
+};
+
+function sourcemapCss(done) {
+  gulp.src([
+      'assets/css/*.min.css'
+    ])
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('assets/css'));
+
+    done();
+};
 
 /** HTML Tasks **/
 
-gulp.task('clean-html', () => {
-    return del.sync([
+function cleanHtml(done) {
+    del.sync([
         paths.html,
         'index.html'
     ], {
         force: true
     });
-});
 
-gulp.task('jade', () => {
-    return gulp.src(paths.jade)
+    done();
+};
+
+function jadeTemplate(done) {
+    gulp.src(paths.jade)
         .pipe(plumber())
         .pipe(jade({
             pretty: true
         }))
         .pipe(gulp.dest('assets/html'))
-});
 
-gulp.task('minify-html', () => {
-    return gulp.src(paths.html)
+        done();
+};
+
+function minifyHtml(done) {
+    gulp.src(paths.html)
         .pipe(plumber())
         .pipe(htmlmin({
             collapseWhitespace: true
         }))
         .pipe(gulp.dest('assets/html'));
-});
 
-gulp.task('move-html', () => {
-    return gulp.src(paths.html)
-        .pipe(plumber())
+    done();
+};
+
+function moveHtml(done) {
+    gulp.src(paths.html)
         .pipe(gulp.dest('.'));
-});
+
+    done();
+};
 
 /** Server Task **/
 
-gulp.task('browserSync', () => {
-    return browserSync.init({
-        server: {
-            baseDir: '.'
-        },
-        port: process.env.PORT || 4790
-    });
-});
+function serve(done) {
+  browserSync.init({
+    server: {
+      baseDir: '.'
+    },
+    port: process.env.PORT || 4790
+  });
+  done();
+};
+
+function reload(done) {
+  browserSync.reload();
+  done();
+};
 
 /** Task Flows **/
 
-gulp.task('js', () => {
-    return runSequence('clean-js', 'typescript', 'minify-js', 'sourcemap-js');
-});
+const js = gulp.series(cleanJs, typescript, minifyJs, sourcemapJs, reload);
 
-gulp.task('css', () => {
-    return runSequence('clean-css', 'scss', 'minify-css', 'sourcemap-css');
-});
+const css = gulp.series(cleanCss, scss, minifyCss, sourcemapCss, reload);
 
-gulp.task('html', () => {
-    return runSequence('clean-html', 'jade', 'minify-html', 'move-html');
-});
+const html = gulp.series(cleanHtml, jadeTemplate, minifyHtml, moveHtml, reload);
 
-gulp.task('minify', () => {
-    return runSequence('css', 'js');
-});
+const minify = gulp.series(css, js);
 
-gulp.task('serve', () => {
-    runSequence('minify', 'browserSync');
-    gulp.watch('assets/css/**/*.scss', ['css', browserSync.reload]);
-    gulp.watch('assets/js/**/*.ts', ['js', browserSync.reload]);
-    gulp.watch('assets/html/**/*.jade', ['html', browserSync.reload]);
-});
+function watch() {
+  gulp.watch('assets/css/**/*.scss', css);
+  gulp.watch('assets/js/**/*.ts', js);
+  gulp.watch('index.html', reload);
+  // gulp.watch('assets/html/**/*.jade', html);
+};
 
-gulp.task('default', ['serve']);
+gulp.task('default', gulp.series(minify, serve, watch));
 
 module.exports = gulp;
